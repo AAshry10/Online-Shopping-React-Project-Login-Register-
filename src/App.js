60b2142225a1f4f4f501f7bb44e8c25e195bad5b
Ahmed El-ashry <115898382+AAshry10@users.noreply.github.com>
2024-09-17 20:09:37 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+
+
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
